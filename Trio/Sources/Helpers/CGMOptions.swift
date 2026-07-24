d6964ff2ec102898441da7b598c27c5439178864
Robert <comps@diprobe.com>
2026-07-24 07:02:34 +0200

    Merge remote-tracking branch 'trioneer-dev/feat/dev-libre3' into dev

--- Trio/Sources/Helpers/CGMOptions.swift
+++ Trio/Sources/Helpers/CGMOptions.swift
@@ -1,4 +1,26 @@
-import Foundation
+let cgmOptions: [CGMOption] = [
+    CGMOption(name: "Dexcom G5", predicate: { $0.type == .plugin && $0.displayName.contains("G5") }),
+    CGMOption(name: "Dexcom G6 / ONE", predicate: { $0.type == .plugin && $0.displayName.contains("G6") }),
+    CGMOption(name: "Dexcom G7 / ONE+", predicate: { $0.type == .plugin && $0.displayName.contains("G7") }),
+    CGMOption(name: "Dexcom Share", predicate: { $0.type == .plugin && $0.displayName.contains("Dexcom Share") }),
+    CGMOption(
+        name: "FreeStyle Libre 1 / 2 / 2+",
+        predicate: { $0.type == .plugin && $0.displayName == "FreeStyle Libre 1 / 2 / 2+" }
+    ),
+    CGMOption(
+        name: "FreeStyle Libre Demo",
+        predicate: { $0.type == .plugin && $0.displayName == "FreeStyle Libre Demo" }
+    ),
+    CGMOption(
+        name: "FreeStyle Libre 3 / 3+",
+        predicate: { $0.type == .plugin && $0.displayName == "FreeStyle Libre 3 / 3+" }
+    ),
+    CGMOption(name: "Eversense", predicate: { $0.type == .plugin && $0.displayName == "Eversense" }),
+    CGMOption(name: "Glucose Simulator", predicate: { $0.type == .simulator }),
+    CGMOption(name: "Medtronic Enlite", predicate: { $0.type == .enlite }),
+    CGMOption(name: "Nightscout as CGM", predicate: { $0.type == .nightscout }),
+    CGMOption(name: "xDrip4iOS", predicate: { $0.type == .xdrip })
+]
 
 var cgmOptions: [CGMOption] {
     var options = [
